98f700023e6e8dfcc92e33715994394e3f019de2
Boukri Amine <AMINE_BOUKRI@emsi-edu.ma>
2022-07-19 15:18:30 +0100

    create Template

---
+++ src/app/components/navbar-vertical/navbar-vertical.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-vertical',
+  templateUrl: './navbar-vertical.component.html',
+  styleUrls: ['./navbar-vertical.component.css']
+})
+export class NavbarVerticalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
